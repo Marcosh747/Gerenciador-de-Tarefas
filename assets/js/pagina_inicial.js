18f1de39059432074d36e41b6efcb2cd2e494a42
Levi <140334035+LeviJJesus@users.noreply.github.com>
2023-10-19 22:14:07 -0300

    atualizações

--- assets/js/pagina_inicial.js
+++ assets/js/pagina_inicial.js
@@ -1,5 +1,5 @@
- // Função para criar uma nova tarefa
- function criarTarefa() {
+// Função para criar uma nova tarefa
+function criarTarefa() {
     const categoriaSelecionada = document.getElementById("categoria").value;
     const data = document.getElementById("data").value;
     const descricao = document.getElementById("descricao-criar").value;
@@ -75,17 +75,10 @@ function exibirTarefasPorCategoria(categoria, feedId) {
         const BtnEditar = document.createElement("button");
         BtnEditar.className = "botoes botao-selecionado-editar";
         BtnEditar.innerHTML = `
-<<<<<<< Updated upstream
             <svg 
                 xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960" width="24">
                 <path fill="#45483D" d="M200-200h57l391-391-57-57-391 391v57Zm-40 80q-17 0-28.5-11.5T120-160v-97q0-16 6-30.5t17-25.5l505-504q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12-11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L313-143q-11 11-25.5 17t-30.5 6h-97Zm600-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
             </svg>
-=======
-        <svg 
-            xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-            <path fill="#45483D" d="M200-200h57l391-391-57-57-391 391v57Zm-40 80q-17 0-28.5-11.5T120-160v-97q0-16 6-30.5t17-25.5l505-504q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12-12 28-12.5t29 11.5l143 143q12 12 12 28t-12 28L669-309q-12 12-28.5 11.5T612-310q-11-12-10.5-28.5T613-366l74-74Z"/>
-        </svg>
->>>>>>> Stashed changes
         `;
 
         const BtnApagar = document.createElement("button");
@@ -121,14 +114,8 @@ function exibirTarefasPorCategoria(categoria, feedId) {
     });
 }
 
-<<<<<<< Updated upstream
 // Chame exibirTarefas inicialmente
 exibirTarefas();
-=======
-exibirTarefasPorCategoria('pessoal', "#feed1");
-exibirTarefasPorCategoria('trabalho', "#feed2");
-exibirTarefasPorCategoria('estudo', "#feed3");
->>>>>>> Stashed changes
 
 // CONTEINER CENTRAL 
 const modal = document.querySelector(".modal-false");
@@ -153,12 +140,8 @@ function pegaUsuario() {
     }
     return "Usuário não encontrado";
 }
-<<<<<<< Updated upstream
 
 // exibir o nome do usuário
-=======
-// Selecione o elemento h2 para exibir o nome do usuário
->>>>>>> Stashed changes
 const h2 = document.querySelector('.ola-usuario');
 
 h2.textContent = "Olá, " + pegaUsuario();
